aa52c59724c899d132d2052a6fceb652b45b052b
daniel <daniel.diego@outlook.com>
2014-01-06 13:28:37 -0600

    Service Layer

---
+++ ASPPatterns.Chap3.Layered.Service/ProductMapperExtensionMethods.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ASPPatterns.Chap3.Layered.Service
+{
+    public static class ProductMapperExtensionMethods
+    {
+        public static IList<ProductViewModel> ConvertToProductListViewModel(this IList<Model.Product> products)
+        {
+            return products.Select(product => product.ConvertToProductViewModel()).ToList();
+        }
+
+        public static ProductViewModel ConvertToProductViewModel(this Model.Product product)
+        {
+            var productViewModel = new ProductViewModel
+            {
+                ProductId = product.Id,
+                Name = product.Name,
+                Rrp = String.Format("{0:C}", product.Price.SellingPrice),
+                SellingPrice = String.Format("{0:C}", product.Price.SellingPrice)
+            };
+
+            if (product.Price.Discount > 0)
+            {
+                productViewModel.Discount = String.Format("{0:C}", product.Price.Discount);
+            }
+
+            if (product.Price.Savings < 1 && product.Price.Savings > 0)
+            {
+                productViewModel.Savings = product.Price.Savings.ToString("#%");
+            }
+
+            return productViewModel;
+        }
+    }
+}
---
+++ ASPPatterns.Chap3.Layered.Service/ProductService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ASPPatterns.Chap3.Layered.Service
+{
+    public class ProductService
+    {
+        private readonly Model.ProductService _productService;
+
+        public ProductService(Model.ProductService productService)
+        {
+            _productService = productService;
+        }
+
+        public ProductListResponse GetAllProductsFor(ProductListRequest productListRequest)
+        {
+            var productListResponse = new ProductListResponse();
+
+            try
+            {
+                var productEntities = _productService.GetAllProductsFor(productListRequest.CustomerType);
+                productListResponse.Products = productEntities.ConvertToProductListViewModel();
+                productListResponse.Succes = true;
+            }
+            catch (Exception)
+            {
+                //Log the exception
+                productListResponse.Succes = false;
+                //Return a friendly error message
+                productListResponse.Message = "An error ocurred.";
+            }
+
+            return productListResponse;
+        }
+    }
+}
